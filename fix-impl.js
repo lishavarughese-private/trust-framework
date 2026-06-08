c7aff95d0646a50cfbd1376cd15c04158b7ccf46
Lisha Varughese <lishavarughese@gmail.com>
2026-06-07 23:03:19 -0400

    Clean up temp files

--- fix-impl.js
+++
@@ -1,8 +0,0 @@
-const fs = require("fs");
-const p = "C:/Users/ashwi/trust-framework/spec-kit/IMPL.json";
-const impl = JSON.parse(fs.readFileSync(p, "utf8"));
-impl.task_coverage["TASK-014"] = "complete";
-impl.task_coverage["TASK-015"] = "complete";
-impl.task_coverage["TASK-016"] = "complete";
-fs.writeFileSync(p, JSON.stringify(impl, null, 2), "utf8");
-console.log("Fixed");
